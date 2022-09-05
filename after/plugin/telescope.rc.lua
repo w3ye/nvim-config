f0e7471e71e1770b97c1b27e93653006794130aa
Will <33244107+w3ye@users.noreply.github.com>
2022-09-04 23:12:08 -0400

    update telescope binding with trouble

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,7 +1,7 @@
 local actions = require("telescope.actions")
+local trouble = require("trouble.providers.telescope")
 
 local telescope = require("telescope")
--- telescope.load_extension("workspaces")
 telescope.load_extension("projects")
 
 telescope.setup {
@@ -10,7 +10,9 @@ telescope.setup {
       i = {
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
-      }
+        ["<C-t>"] = trouble.open_with_trouble,
+      },
+      n = { ["<C-t>"] = trouble.open_with_trouble },
     },
   },
   extensions = {
