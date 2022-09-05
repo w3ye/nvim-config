3cce25a8978009216adc5d18e6a68a7b6576b66f
Will <33244107+w3ye@users.noreply.github.com>
2022-09-05 11:29:02 -0400

    os checks in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+if [["$OSTYPE" == "*linux*"]]; then
+  sudo apt install figlet
+fi
 
-# packer install
+if [["$OSTYPE" == "darwin"]]; then
+  brew install figlet
+fi
+
+# install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
