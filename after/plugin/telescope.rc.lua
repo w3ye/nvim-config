15a9dc9547555c90b5f1538c001a8fcad73d3a0a
William Ye <33244107+w3ye@users.noreply.github.com>
2022-09-06 01:08:45 -0400

    add ignored files in file finder

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -4,20 +4,25 @@ local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 telescope.load_extension("projects")
 
-telescope.setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-t>"] = trouble.open_with_trouble,
-      },
-      n = { ["<C-t>"] = trouble.open_with_trouble },
-    },
-  },
-  extensions = {
-    -- workspaces = {
-    --   keep_insert = true,
-    -- }
-  }
-}
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-t>"] = trouble.open_with_trouble,
+			},
+			n = { ["<C-t>"] = trouble.open_with_trouble },
+		},
+	},
+	file_ignore_patterns = {
+		"node_modules",
+		".git",
+		".editorconfig",
+	},
+	extensions = {
+		-- workspaces = {
+		--   keep_insert = true,
+		-- }
+	},
+})
