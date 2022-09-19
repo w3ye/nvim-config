1f061349867481525341c0654f77324202c1e59f
Will <33244107+w3ye@users.noreply.github.com>
2022-09-19 00:19:42 -0400

    split telescope file browser with ag

--- after/plugin/telescope_plugin.rc.lua
+++ after/plugin/telescope_plugin.rc.lua
@@ -1,23 +1,9 @@
 local telescope = require("telescope")
-local lga_actions = require("telescope-live-grep-args.actions")
-local actions = require("telescope.actions")
 telescope.load_extension("file_browser")
 telescope.load_extension("live_grep_args")
 
 telescope.setup({
 	extensions = {
-		live_grep_args = {
-			auto_quoting = true, -- enable/disable auto-quoting
-			-- override default mappings
-			-- default_mappings = {},
-			mappings = { -- extend mappings
-				i = {
-					["<C-l>"] = lga_actions.quote_prompt(),
-					["<C-k>"] = actions.move_selection_previous,
-				},
-			},
-		},
-
 		file_browser = {
 			theme = "dropdown",
 			-- disables netrw and use telescope-file-browser in its place
