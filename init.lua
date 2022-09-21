89c99d15adb50723f84b54bd92af2a0eb2b683c8
William Ye <33244107+w3ye@users.noreply.github.com>
2022-09-21 09:49:05 -0400

    remove mouseevent

--- init.lua
+++ init.lua
@@ -29,19 +29,18 @@ vim.opt.splitbelow = true
 vim.opt.hlsearch = false
 vim.opt.relativenumber = true
 vim.opt.cursorline = false
-vim.opt.mousemoveevent = true
 
 -- code folding
 vim.opt.foldlevel = 20
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.api.nvim_create_autocmd("BufRead", {
-    callback = function()
-        vim.api.nvim_create_autocmd("BufWinEnter", {
-            once = true,
-            command = "normal! zx",
-        })
-    end,
+	callback = function()
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			once = true,
+			command = "normal! zx",
+		})
+	end,
 })
 
 vim.cmd([[colorscheme nightfox]])
@@ -140,17 +139,17 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 -- LSP disagnostic icons
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- diagnostic settings
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
 -- same word highlighting
