9e9b96078d604158cf351a43032f0e490549c2ef
William Ye <33244107+w3ye@users.noreply.github.com>
2022-10-03 11:17:02 -0400

    bufferline offset -> api

--- after/plugin/tabline.rc.lua
+++ after/plugin/tabline.rc.lua
@@ -1,6 +1,6 @@
 local ok, bufferline = pcall(require, "bufferline")
 if not ok then
-	return
+  return
 end
 
 local map = vim.api.nvim_set_keymap
@@ -47,41 +47,41 @@ map("n", "<leader>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
 bufferline.setup()
+local bufferline_state = require("bufferline.api")
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = "*",
-	callback = function()
-		if vim.bo.filetype == "NvimTree" then
-			require("bufferline.state").set_offset(31, "FileTree")
-		end
-	end,
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype == "NvimTree" then
+      require("bufferline.api").set_offset(31, "FileTree")
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWinLeave", {
-	pattern = "*",
-	callback = function()
-		if vim.fn.expand("<afile>"):match("NvimTree") then
-			require("bufferline.state").set_offset(0)
-		end
-	end,
+  pattern = "*",
+  callback = function()
+    if vim.fn.expand("<afile>"):match("NvimTree") then
+      require("bufferline.api").set_offset(0)
+    end
+  end,
 })
 
 -- NOTE: push tabline to the right when nvimTree is open
 local nvim_tree_events = require("nvim-tree.events")
-local bufferline_state = require("bufferline.state")
 
 local function get_tree_size()
-	return require("nvim-tree.view").View.width
+  return require("nvim-tree.view").View.width
 end
 
 nvim_tree_events.subscribe("TreeOpen", function()
-	bufferline_state.set_offset(get_tree_size())
+  bufferline_state.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe("Resize", function()
-	bufferline_state.set_offset(get_tree_size())
+  bufferline_state.set_offset(get_tree_size())
 end)
 
 nvim_tree_events.subscribe("TreeClose", function()
-	bufferline_state.set_offset(0)
+  bufferline_state.set_offset(0)
 end)
