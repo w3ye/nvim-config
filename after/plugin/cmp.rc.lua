2eacadf9e8dbd8c1cb706c49f0d750b40c7298cc
William Ye <33244107+w3ye@users.noreply.github.com>
2022-10-25 11:53:23 -0400

    update cmp variables in lsp

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -3,96 +3,95 @@ local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-r>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		["<Tab>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<C-e>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<C-q>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-r>"] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ["<Tab>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-e>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-q>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
 
-	-- disable completion during comments
-	enabled = function()
-		local context = require("cmp.config.context")
-		if
-			vim.api.nvim_get_mode().mode == "c"
-			or vim.api.nvim_buf_get_option(0, "buftype") == ("prompt" or "nofile")
-			or vim.bo.filetype == "gitcommit"
-		then
-			return false
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("comment")
-		end
-	end,
+  -- disable completion during comments
+  enabled = function()
+    -- local context = require("cmp.config.context")
+    -- if vim.api.nvim_get_mode().mode == "c"
+    --     or vim.api.nvim_buf_get_option(0, "buftype") == ("prompt" or "nofile")
+    --     or vim.bo.filetype == "gitcommit"
+    -- then
+    --   return false
+    -- else
+    --   return not context.in_treesitter_capture("comment") and not context.in_syntax_group("comment")
+    -- end
+  end,
 
-	formatting = {
-		format = lspkind.cmp_format({
-			mode = "symbol_text",
-			maxwidth = 50,
-			menu = {
-				buffer = "[buffer]",
-				nvim_lsp = "[lsp]",
-				luasnip = "[lua_snip]",
-			},
-		}),
-	},
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol_text",
+      maxwidth = 50,
+      menu = {
+        buffer = "[buffer]",
+        nvim_lsp = "[lsp]",
+        luasnip = "[lua_snip]",
+      },
+    }),
+  },
 
-	sources = cmp.config.sources({
-		{ name = "luasnip", options = { use_show_condition = false } }, -- For luasnip users.
-		{
-			name = "nvim_lsp",
-			entry_filter = function(entry, ctx)
-				return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-			end,
-		},
-		{ name = "path" },
-	}, {
-		{ name = "buffer", keyword_length = 3 },
-	}),
-	experimental = { ghost_text = true },
+  sources = cmp.config.sources({
+    { name = "luasnip", options = { use_show_condition = false } }, -- For luasnip users.
+    {
+      name = "nvim_lsp",
+      entry_filter = function(entry, ctx)
+        return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+      end,
+    },
+    { name = "path" },
+  }, {
+    { name = "buffer", keyword_length = 3 },
+  }),
+  experimental = { ghost_text = true },
 })
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline("/", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
