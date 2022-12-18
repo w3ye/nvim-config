4e82b876970c69361a24ea1bfe667a42381759ff
Will <33244107+w3ye@users.noreply.github.com>
2022-12-18 13:20:00 -0500

    update treesitter

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -26,6 +26,57 @@ require("nvim-treesitter.configs").setup({
 			},
 		},
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>", -- set to `false` to disable one of the mappings
+			node_incremental = "<C-space>",
+			scope_incremental = "<C-s>",
+			node_decremental = "<C-z>",
+		},
+	},
+	indent = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["<leader>mn"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["<leader>mN"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["<leader>mp"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+	},
 	textsubjects = {
 		enable = true,
 		prev_selection = ",", -- (Optional) keymap to select the previous selection
