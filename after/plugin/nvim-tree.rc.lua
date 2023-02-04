bd5bdb9dbaccbcc9fcdcd0aab9d15a24a5532400
William Ye <33244107+w3ye@users.noreply.github.com>
2023-02-03 21:47:17 -0500

    add to nvim tree

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -3,11 +3,25 @@ if not ok then
 	return
 end
 
+local api = require("nvim-tree.api")
+api.events.subscribe(api.events.Event.FileCreated, function(file)
+	vim.cmd("edit " .. file.fname)
+end)
+
 tree.setup({
+	live_filter = {
+		prefix = "[FILTER]: ",
+		always_show_folders = false, -- Turn into false from true by default
+	},
 	filters = {
 		dotfiles = false,
 	},
 	view = {
 		adaptive_size = true,
+	},
+	mappings = {
+		list = {
+			{ key = "C", action_cb = collapse_all },
+		},
 	},
 })
