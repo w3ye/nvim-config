67dd5f1f7b89b147402530371f9600446a752b17
Will <33244107+w3ye@users.noreply.github.com>
2023-02-06 00:42:08 -0500

    lualine: use round seprarators

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -1,97 +1,110 @@
 local ok, lualine = pcall(require, "lualine")
 if not ok then
-  return
+	return
 end
 
 local navic = require("nvim-navic")
 
 local colors = {
-  bg = "#202328",
-  fg = "#bbc2cf",
-  yellow = "#ECBE7B",
-  cyan = "#008080",
-  darkblue = "#081633",
-  green = "#98be65",
-  orange = "#FF8800",
-  violet = "#a9a1e1",
-  magenta = "#c678dd",
-  blue = "#51afef",
-  red = "#ec5f67",
+	bg = "#202328",
+	fg = "#bbc2cf",
+	yellow = "#ECBE7B",
+	cyan = "#008080",
+	darkblue = "#081633",
+	green = "#98be65",
+	orange = "#FF8800",
+	violet = "#a9a1e1",
+	magenta = "#c678dd",
+	blue = "#51afef",
+	red = "#ec5f67",
 }
 
 lualine.setup({
-  options = {
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    },
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diagnostics" },
-    lualine_c = {
-      {
-        "filename",
-        path = 1,
-        symbols = {
-          modified = " ●",
-        },
-      },
-    },
-    -- lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_x = {
-      {
-        "lsp_progress",
-        display_components = { "lsp_client_name" },
-      },
-      {
-        "diff",
-        diff_color = {
-          added = { fg = colors.green },
-          modified = { fg = colors.orange },
-          removed = { fg = colors.red },
-        },
-        symbols = {
-          added = " ",
-          modified = " ",
-          removed = " ",
-        },
-      },
-      "filetype",
-    },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  winbar = {
-    lualine_c = {
-      { navic.get_location, cond = navic.is_available },
-    },
-  },
-  inactive_winbar = {
-    lualine_c = {
-      { navic.get_location, cond = navic.is_available },
-    },
-  },
-  extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		-- component_separators = { left = "", right = "" },
+		component_separators = { left = ")", right = "(" },
+		section_separators = { left = "", right = "" },
+		--  section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = {
+			"branch",
+			{
+				"diff",
+				diff_color = {
+					added = { fg = colors.green },
+					modified = { fg = colors.orange },
+					removed = { fg = colors.red },
+				},
+				symbols = {
+					added = " ",
+					modified = " ",
+					removed = " ",
+				},
+			},
+		},
+		lualine_c = {
+			{
+				"filename",
+				path = 1,
+				symbols = {
+					modified = " ●",
+				},
+			},
+		},
+		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_x = {
+			{
+				"lsp_progress",
+				display_components = { "lsp_client_name" },
+			},
+			"diagnostics",
+		},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+
+	inactive_sections = {
+		lualine_a = {
+			{
+				"filename",
+				path = 1,
+				symbols = {
+					modified = " ●",
+				},
+			},
+		},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {
+		lualine_c = {
+			{ navic.get_location, cond = navic.is_available },
+		},
+	},
+	inactive_winbar = {
+		lualine_c = {
+			{ navic.get_location, cond = navic.is_available },
+		},
+	},
+	extensions = {},
 })
