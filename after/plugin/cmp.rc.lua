a95dadd5ef647cc9d782a5ece9cb16b5401e1af5
Will <33244107+w3ye@users.noreply.github.com>
2023-02-05 23:26:11 -0500

    fix formatting

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -5,68 +5,65 @@ local cmp_keymaps = require("will.keymaps.cmp-keymaps")
 local types = require("cmp.types")
 
 cmp.setup({
-	sources = cmp.config.sources({
-		{ name = "luasnip" }, -- For luasnip users.
-		{
-			name = "nvim_lsp",
-			entry_filter = function(entry, ctx)
-				return types.lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-			end,
-		},
-		{ name = "buffer", keyword_length = 3 },
-		{ name = "path" },
-	}),
-	mapping = cmp_keymaps,
-	snippet = {
-		cmp.config.sources({
-			{ name = "luasnip" }, -- For luasnip users.
-			{
-				name = "nvim_lsp",
-				entry_filter = function(entry, ctx)
-					return types.lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-				end,
-			},
-			{ name = "buffer", keyword_length = 3 },
-			{ name = "path" },
-		}),
-	},
-	enabled = function()
-		local context = require("cmp.config.context")
-		if
-			vim.api.nvim_get_mode().mode == "c"
-			or vim.api.nvim_buf_get_option(0, "buftype") == ("prompt" or "nofile")
-			or vim.bo.filetype == "gitcommit"
-		then
-			return false
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("comment")
-		end
-	end,
+  sources = cmp.config.sources({
+    { name = "luasnip" }, -- For luasnip users.
+    {
+      name = "nvim_lsp",
+      entry_filter = function(entry, ctx)
+        return types.lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+      end,
+    },
+    { name = "buffer", keyword_length = 3 },
+    { name = "path" },
+  }),
+  mapping = cmp_keymaps,
+  snippet = {
+    cmp.config.sources({
+      { name = "luasnip" }, -- For luasnip users.
+      {
+        name = "nvim_lsp",
+        entry_filter = function(entry, ctx)
+          return types.lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+        end,
+      },
+      { name = "buffer", keyword_length = 3 },
+      { name = "path" },
+    }),
+  },
+  enabled = function()
+    local context = require("cmp.config.context")
+    if vim.api.nvim_get_mode().mode == "c"
+        or vim.api.nvim_buf_get_option(0, "buftype") == ("prompt" or "nofile")
+        or vim.bo.filetype == "gitcommit"
+    then
+      return false
+    else
+      return not context.in_treesitter_capture("comment") and not context.in_syntax_group("comment")
+    end
+  end,
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline({
-
-		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-	}),
-	sources = {
-		{ name = "buffer" },
-	},
-	enabled = true,
+  mapping = cmp.mapping.preset.cmdline({
+    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+  }),
+  sources = {
+    { name = "buffer" },
+  },
+  enabled = true,
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline({
-
-		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+  mapping = cmp.mapping.preset.cmdline({
+    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
