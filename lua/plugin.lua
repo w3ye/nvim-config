2428f670ac6a978d3f7e4b890b2e7599145d7083
Will <33244107+w3ye@users.noreply.github.com>
2023-04-09 23:32:14 -0400

    add lspsaga

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -56,6 +56,15 @@ require("lazy").setup({
 		"mg979/vim-visual-multi",
 		"AckslD/nvim-trevJ.lua",
 		"folke/trouble.nvim",
+		{
+			"glepnir/lspsaga.nvim",
+			event = "LspAttach",
+			dependencies = {
+				{ "nvim-tree/nvim-web-devicons" },
+				--Please make sure you install markdown and markdown_inline parser
+				{ "nvim-treesitter/nvim-treesitter" },
+			},
+		},
 	},
 	-- git
 	{
