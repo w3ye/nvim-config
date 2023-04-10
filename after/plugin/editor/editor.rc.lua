f0d2ac51f12c264ed0d5370b31bd0dcacfc531da
Will <33244107+w3ye@users.noreply.github.com>
2023-04-09 23:57:14 -0400

    init lspsaga init

---
+++ after/plugin/editor/editor.rc.lua
@@ -0,0 +1,17 @@
+require("nvim-lastplace").setup({
+  lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+  lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+  lastplace_open_folds = true,
+})
+
+require("flit").setup({
+  keys = { f = "f", F = "F", t = "t", T = "T" },
+  -- A string like "nv", "nvo", "o", etc.
+  labeled_modes = "nv",
+  multiline = true,
+  -- Like `leap`s similar argument (call-specific overrides).
+  -- E.g.: opts = { equivalence_classes = {} }
+  opts = {},
+})
+
+require("leap").set_default_keymaps()
---
+++ after/plugin/ui/ui.rc.lua
@@ -0,0 +1,23 @@
+-- indent
+vim.opt.termguicolors = true
+vim.opt.list = true
+
+require("indent_blankline").setup({
+	show_trailing_blankline_indent = false,
+	show_current_context = false,
+	filetype_exclude = {
+		"startify",
+		"help",
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	callback = function()
+		vim.b.miniindentscope_disable = true
+	end,
+})
+require("mini.indentscope").setup({
+	symbol = "│",
+	options = { try_as_border = true },
+})
