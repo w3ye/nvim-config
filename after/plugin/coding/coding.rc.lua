e1f59708b6e0e06129cbf31f2921aac4c206829d
Will <33244107+w3ye@users.noreply.github.com>
2023-04-11 01:09:21 -0400

    update autopairs config

--- after/plugin/coding/coding.rc.lua
+++ after/plugin/coding/coding.rc.lua
@@ -1,6 +1,17 @@
-require("nvim-autopairs").setup()
+require("nvim-autopairs").setup({
+	fast_wrap = {
+		map = "<M-e>",
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+		end_key = "$",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		highlight = "Search",
+		highlight_grey = "Comment",
+	},
+})
 
 require("Comment").setup({
-  ignore = "^$",
-  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	ignore = "^$",
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
