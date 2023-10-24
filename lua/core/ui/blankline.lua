6a96de4e558cdb4e63a424b341e45248eb9c454c
Will <33244107+w3ye@users.noreply.github.com>
2023-10-24 00:13:56 -0400

    update nvim and package versions

--- lua/core/ui/blankline.lua
+++ lua/core/ui/blankline.lua
@@ -1,13 +1,18 @@
 local M = {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	config = function()
-		require("indent_blankline").setup({
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-			filetype_exclude = {
-				"startify",
-				"help",
-				"dashboard",
+		require("ibl").setup({
+			-- show_trailing_blankline_indent = false,
+			-- show_current_context = false,
+		})
+		require("ibl").overwrite({
+			exclude = {
+				filetypes = {
+					"startify",
+					"help",
+					"dashboard",
+				},
 			},
 		})
 	end,
