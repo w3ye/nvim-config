953271fcf7fa686bc5125f5ee5a108eed5c64136
Will <33244107+w3ye@users.noreply.github.com>
2023-11-29 19:29:42 -0500

    add flash

--- lua/core/editor/init.lua
+++ lua/core/editor/init.lua
@@ -14,26 +14,24 @@ local minor_setup = {
 		end,
 	},
 	{
-		"ggandor/flit.nvim",
-		enabled = enabled.flit,
-		config = function()
-			require("flit").setup({
-				keys = { f = "f", F = "F", t = "t", T = "T" },
-				-- A string like "nv", "nvo", "o", etc.
-				labeled_modes = "nv",
-				multiline = true,
-				-- Like `leap`s similar argument (call-specific overrides).
-				-- E.g.: opts = { equivalence_classes = {} }
-				opts = {},
-			})
-		end,
-	},
-	{
-		"ggandor/leap.nvim",
-		enabled = enabled.leap,
-		config = function()
-			require("leap").set_default_keymaps()
-		end,
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		enabled = _G.enabled.flash,
+		opts = {
+			modes = {
+				search = {
+					enabled = false,
+				},
+			},
+		},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
 	},
 	{
 		"folke/which-key.nvim",
