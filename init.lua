09f4c9c1412d301ac2a12007ea3fdca15f4a47d1
William Ye <33244107+w3ye@users.noreply.github.com>
2024-05-06 22:38:51 -0400

    remove virtual text from init

--- init.lua
+++ init.lua
@@ -81,9 +81,6 @@ vim.cmd("set nolist")
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-	vim.diagnostic.config({
-		virtual_text = false,
-	})
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
