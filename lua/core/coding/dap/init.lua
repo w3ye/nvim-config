e93bbfe7291b715340d6567143644851e70a2cd4
William Ye <33244107+w3ye@users.noreply.github.com>
2024-05-06 22:38:33 -0400

    enable js dap again

--- lua/core/coding/dap/init.lua
+++ lua/core/coding/dap/init.lua
@@ -1,18 +1,55 @@
-local js = require("core.coding.dap.js").dap
-local js_adapter = require("core.coding.dap.js").adapter
-local js_configurations = require("core.coding.dap.js").configurations
+local js = require("core.coding.dap.js")
+-- local js_adapter = require("core.coding.dap.js").adapter
+-- local js_configurations = require("core.coding.dap.js").configurations
 local enabled = _G.enabled.dap
 
 local M = {
+	{
+		"microsoft/vscode-js-debug",
+		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+	},
 	{
 		"mfussenegger/nvim-dap",
 		lazy = true,
 		-- config = require("core.coding.debugger").setup(),
 		config = function()
-			require("core.coding.debugger").setup()
-			js_adapter()
-			js_configurations()
+			-- require("core.coding.debugger").setup()
+			require("dap").adapters["pwa-node"] = {
+				type = "server",
+				host = "localhost",
+				port = "${port}",
+				executable = {
+					command = "js-debug-adapter", -- As I'm using mason, this will be in the path
+					args = { "${port}" },
+				},
+			}
+
+			for _, language in ipairs({ "typescript", "javascript" }) do
+				require("dap").configurations[language] = {
+					{
+						name = "Debug Platform Model",
+						type = "pwa-node",
+						request = "attach",
+						remoteRoot = "/usr/src/app",
+						localRoot = "${workspaceFolder}",
+						-- protocol = "inspector",
+						port = 9229,
+						restart = true,
+						address = "0.0.0.0",
+						skipFiles = { "<node_internals>/**" },
+						sourceMaps = true,
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach to node",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
 		end,
+
 		enabled = enabled,
 		keys = {
 			{ "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", desc = "Toggle breakpoint" },
@@ -41,18 +78,99 @@ local M = {
 				"<Leader>bs",
 				function()
 					local widgets = require("dap.ui.widgets")
-					widgets.centered_float(widgets.scopes)
+					widgets.sidebar(widgets.scopes).open()
 				end,
 			},
 		},
 	},
 	{
 		"rcarriga/nvim-dap-ui",
-		lazy = true,
+		-- lazy = true,
+		lazy = false,
 		enabled = enabled,
 		dependencies = { "mfussenegger/nvim-dap" },
 		config = function()
-			require("dapui").setup()
+			require("dapui").setup({
+				controls = {
+					element = "repl",
+					enabled = true,
+					icons = {
+						disconnect = "",
+						pause = "",
+						play = "",
+						run_last = "",
+						step_back = "",
+						step_into = "",
+						step_out = "",
+						step_over = "",
+						terminate = "",
+					},
+				},
+				element_mappings = {},
+				expand_lines = true,
+				floating = {
+					border = "single",
+					mappings = {
+						close = { "q", "<Esc>" },
+					},
+				},
+				force_buffers = true,
+				icons = {
+					collapsed = "",
+					current_frame = "",
+					expanded = "",
+				},
+				layouts = {
+					{
+						elements = {
+							{
+								id = "scopes",
+								size = 0.25,
+							},
+							{
+								id = "watches",
+								size = 0.25,
+							},
+							{
+								id = "breakpoints",
+								size = 0.25,
+							},
+							{
+								id = "stacks",
+								size = 0.25,
+							},
+						},
+						position = "left",
+						size = 60,
+					},
+					{
+						elements = {
+							{
+								id = "repl",
+								size = 0.25,
+							},
+							{
+								id = "console",
+								size = 0.25,
+							},
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
+				mappings = {
+					edit = "e",
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					repl = "r",
+					toggle = "t",
+				},
+				render = {
+					indent = 1,
+					max_value_lines = 100,
+				},
+			})
 
 			local dap, dapui = require("dap"), require("dapui")
 
@@ -70,7 +188,7 @@ local M = {
 			{ "<leader>ui", "<cmd>lua require'dapui'.toggle()<cr>", desc = "Toggle DAP UI" },
 		},
 	},
-	js,
+	-- js,
 }
 
 return M
--- lua/core/coding/dap/js.lua
+++ lua/core/coding/dap/js.lua
@@ -4,88 +4,51 @@ if _G.env == nil then
 end
 
 local M = {
-	dap = {
-		"mxsdev/nvim-dap-vscode-js",
-		enabled = _G.enabled.dap,
+	{
+		"microsoft/vscode-js-debug",
+		opt = true,
 		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+	},
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		dependencies = { "mfussenegger/nvim-dap" },
 		config = function()
 			require("dap-vscode-js").setup({
-				node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-				debugger_path = _G.env.js_debugger_path, -- Path to vscode-js-debug installation.
-				debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-				adapters = {
-					"chrome",
-					"pwa-node",
-					"pwa-chrome",
-					"pwa-msedge",
-					"node-terminal",
-					"pwa-extensionHost",
-					"node",
-					"chrome",
-				}, -- which adapters to register in nvim-dap
+				-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+				debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
 				-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
 				-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
 				-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
 			})
+
+			for _, language in ipairs({ "typescript", "javascript" }) do
+				require("dap").configurations[language] = {
+					{
+						name = "Debug Platform Model",
+						type = "pwa-node",
+						request = "attach",
+						remoteRoot = "/usr/src/app",
+						localRoot = "${workspaceFolder}/platform-components",
+						protocol = "inspector",
+						port = 9229,
+						restart = true,
+						address = "0.0.0.0",
+						skipFiles = { "<node_internals>/**" },
+						sourceMaps = true,
+					},
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
 		end,
 	},
-
-	adapter = function()
-		require("dap").adapters["pwa-node"] = {
-			type = "server",
-			host = "localhost",
-			port = "${port}",
-			executable = {
-				command = "node",
-				-- 💀 Make sure to update this path to point to your installation
-				args = { "~/.config/nvim/vscode-js-debug-1.82.0/src/dapDebugServer.ts", "${port}" },
-			},
-		}
-	end,
-
-	configurations = function()
-		local js_based_languages = { "typescript", "javascript", "typescriptreact" }
-
-		for _, language in ipairs(js_based_languages) do
-			require("dap").configurations[language] = {
-				{
-					name = "Debug Platform Model",
-					type = "pwa-node",
-					request = "attach",
-					remoteRoot = "/usr/src/app",
-					localRoot = "${workspaceFolder}/platform-components",
-					protocol = "inspector",
-					port = 9229,
-					restart = true,
-					address = "0.0.0.0",
-					skipFiles = { "<node_internals>/**" },
-					sourceMaps = true,
-				},
-				{
-					type = "pwa-node",
-					request = "launch",
-					name = "Launch file",
-					program = "${file}",
-					cwd = "${workspaceFolder}",
-				},
-				{
-					type = "pwa-node",
-					request = "attach",
-					name = "Attach",
-					processId = require("dap.utils").pick_process,
-					cwd = "${workspaceFolder}",
-				},
-				{
-					type = "pwa-chrome",
-					request = "launch",
-					name = 'Start Chrome with "localhost"',
-					url = "http://localhost:3000",
-					webRoot = "${workspaceFolder}",
-					userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
-				},
-			}
-		end
-	end,
 }
 
 return M
