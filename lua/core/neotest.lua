b9de429bc32eb4740142c919d8123cb906a5d5b4
William Ye <33244107+w3ye@users.noreply.github.com>
2024-05-15 11:11:22 -0400

    neotest

--- lua/core/neotest.lua
+++ lua/core/neotest.lua
@@ -1,23 +1,23 @@
-local M = {}
-
-M.neotest_setup = function()
-    require("neotest").setup({
-        adapters = {
-            require("neotest-playwright").adapter({
-                options = {
-                    persist_project_selection = true,
-                    enable_dynamic_test_discovery = true,
-                },
-            }),
-        },
-    })
-end
-
-M.keys = {
-    { "<leader>nts", '<cmd>lua require("neotest").summary.toggle()<cr>',            desc = "Toggle neotest summary" },
-    { "<leader>nr",  '<cmd>lua require("neotest").run.run()',                       desc = "Run closest test" },
-    { "<leader>nra", '<cmd>lua require("neotest").run.attach()',                    desc = "Attach closest test" },
-    { "<leader>nrr", '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<cr>', desc = "Run the whole file" },
-}
-
-return M
+-- local M = {}
+--
+-- M.neotest_setup = function()
+--     require("neotest").setup({
+--         adapters = {
+--             require("neotest-playwright").adapter({
+--                 options = {
+--                     persist_project_selection = true,
+--                     enable_dynamic_test_discovery = true,
+--                 },
+--             }),
+--         },
+--     })
+-- end
+--
+-- M.keys = {
+--     { "<leader>nts", '<cmd>lua require("neotest").summary.toggle()<cr>',            desc = "Toggle neotest summary" },
+--     { "<leader>nr",  '<cmd>lua require("neotest").run.run()',                       desc = "Run closest test" },
+--     { "<leader>nra", '<cmd>lua require("neotest").run.attach()',                    desc = "Attach closest test" },
+--     { "<leader>nrr", '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<cr>', desc = "Run the whole file" },
+-- }
+--
+-- return M
