9ac6d7c84ce314039154aab7f0fc9abe19a74bee
Will <33244107+w3ye@users.noreply.github.com>
2024-05-20 19:21:04 -0400

    add conditional breakpoints

--- lua/core/coding/debugger/init.lua
+++ lua/core/coding/debugger/init.lua
@@ -2,6 +2,11 @@ local js = require("core.coding.debugger.js")
 local ui = require("core.coding.debugger.ui")
 local enabled = _G.enabled.dap
 
+function conditional_breakpoint()
+	local condition = vim.fn.input("Condition: ")
+	require("dap").set_breakpoint(condition, nil, nil)
+end
+
 local M = {
 	{
 		"microsoft/vscode-js-debug",
@@ -16,6 +21,7 @@ local M = {
 		enabled = enabled,
 		keys = {
 			{ "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", desc = "Toggle breakpoint" },
+			{ "<leader>bc", conditional_breakpoint, desc = "Conditional breakpoint" },
 			{ "<F5>", "<cmd>lua require'dap'.continue()<cr>", desc = "Debug continue" },
 			{ "<F7>", "<cmd>lua require'dap'.step_into()<cr>", desc = "Debug step into" },
 			{ "<F8>", "<cmd>lua require'dap'.step_over()<cr>", desc = "Debug step over" },
