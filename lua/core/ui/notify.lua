3ffd2ac5d7e955be8218d40e88360f4754f43533
Will <33244107+w3ye@users.noreply.github.com>
2024-05-21 00:29:22 -0400

    notify move config table to opts

--- lua/core/ui/notify.lua
+++ lua/core/ui/notify.lua
@@ -1,22 +1,23 @@
 local M = {
 	"rcarriga/nvim-notify",
 	enabled = _G.enabled.notify,
-	config = function()
+	config = function(_, opts)
 		vim.opt.termguicolors = true
-		require("notify").setup({
-			timeout = 2000,
-			animation = "fade_in_slide_out",
-			render = "minimal",
-			col = 10,
-			opacity = 100,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-		})
+		require("notify").setup(opts)
 	end,
+	opts = {
+		timeout = 2000,
+		animation = "fade_in_slide_out",
+		render = "compact",
+		col = 10,
+		opacity = 100,
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
+	},
 }
 
 return M
