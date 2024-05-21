cdeffe907729e4280be978cf7088b8a7c6aa41c4
Will <33244107+w3ye@users.noreply.github.com>
2024-05-21 00:25:43 -0400

    move setup logic to opts

--- lua/core/ui/noice.lua
+++ lua/core/ui/noice.lua
@@ -36,39 +36,40 @@ local routes = {
 local M = {
 	"folke/noice.nvim",
 	lazy = false,
-	config = function()
-		require("noice").setup({
-			views = {
-				cmdline_popup = {
-					border = {
-						style = "none",
-						padding = { 2, 3 },
-					},
-					filter_options = {},
-					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-					},
-				},
-			},
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			-- you can enable a preset for easier configuration
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-			routes = routes,
-		})
+	config = function(_, opts)
+		require("noice").setup(opts)
 	end,
+	opts = {
+		views = {
+			cmdline_popup = {
+				border = {
+					style = "none",
+					padding = { 2, 3 },
+				},
+				filter_options = {},
+				win_options = {
+					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+				},
+			},
+		},
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+		routes = routes,
+	},
 	enabled = _G.enabled.noice,
 	keys = {
 		{
@@ -95,8 +96,8 @@ local M = {
 -- vim.keymap.set("c", "<leader>nc", function()
 -- 	require("noice").redirect(vim.fn.getcmdline())
 -- end)
-
+--
 -- vim.keymap.set("n", "<leader>u", function()
---   require("notify").dismiss({ silent = true, pending = true })
+-- 	require("notify").dismiss({ silent = true, pending = true })
 -- end)
 return M
